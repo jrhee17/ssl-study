99ec60c99ba0728858c93e88d842a29a8084d24d
guins_j <guins_j@guins.org>
2019-12-24 22:39:06 +0900

    add permuate, xor, bit manipulation methods

--- before/ch02/des.c
+++ before/ch02/des.c
@@ -4,3 +4,27 @@
 #include <assert.h>
 #include "des.h"
 #include "hex.h"
+
+#define GET_BIT(array, bit) \
+(array[(int) (bit / 8)] & (0x80 >> (bit % 8)))
+#define SET_BIT(array, bit) \
+(array[(int) (bit / 8)] |= (0x80 >> (bit % 8)))
+#define CLEAR_BIT(array, bit) \
+(array[(int) (bit / 8)] &= ~(0x80 >> (bit % 8)))
+
+static void xor(unsigned char *target, const unsigned char *src, int len) {
+    while (len--) {
+        *target++ ^= *src++;
+    }
+}
+
+static void permute(unsigned char target[], const unsigned char src[], const int permute_table[], int len) {
+    int i;
+    for (i = 0; i < len * 8; i++) {
+        if (GET_BIT(src, (permute_table[i] - 1))) {
+            SET_BIT(target, i);
+        } else {
+            CLEAR_BIT(target, i);
+        }
+    }
+}
