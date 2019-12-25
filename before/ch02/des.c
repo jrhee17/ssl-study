0eaa4d51ad1ede4b3999a356610d1b7d20328f3e
guins_j <guins_j@guins.org>
2019-12-25 23:01:57 +0900

    add initial permutation table/method

--- before/ch02/des.c
+++ before/ch02/des.c
@@ -28,3 +28,25 @@ static void permute(unsigned char target[], const unsigned char src[], const int
         }
     }
 }
+
+// terse initial permutation
+static void terse_initial_permute(unsigned char input[], unsigned char output[]) {
+    int i;
+    for (i = 1; i != 8; i = (i + 2) % 9) {
+        for (int j = 7; j >= 0; j--) {
+            output[(i % 2) ? ((i - 1) >> 1) : ((4 + (i >> 1)))] |=
+                    ((input[j] & (0x80 >> i)) >> (7 - i)) << j;
+        }
+    }
+}
+
+// initial permutation table
+static const int ip_table[] = {
+        58, 50, 42, 34, 26, 18, 10, 2,
+        60, 52, 44, 36, 28, 20, 12, 4,
+        62, 54, 46, 38, 30, 22, 14, 6,
+        64, 56, 48, 40, 32, 24, 16, 8,
+        57, 49, 41, 33, 25, 17, 9,  1,
+        59, 51, 43, 35, 27, 19, 11, 3,
+        61, 53, 45, 37, 29, 21, 13, 5,
+        63, 55, 47, 39, 31, 23, 15, 7 };
